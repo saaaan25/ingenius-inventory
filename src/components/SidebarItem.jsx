5186b400325b2c6ff41d2ce7162c550754a28770
sofia <ssarmiento088@gmail.com>
2025-02-08 01:22:30 -0500

    Setting new font

--- src/components/SidebarItem.jsx
+++ src/components/SidebarItem.jsx
@@ -8,7 +8,7 @@ const SidebarItem = ({ title, route, Icon }) => {
     return (
         <button onClick={goToPage} className="bg-selected text-white py-4 pl-10 w-full flex items-center gap-x-3">
             <Icon size={25}/>
-            <p className="text-xl font-medium">{title}</p>
+            <p className="text-lg">{title}</p>
         </button>
     );
 }
