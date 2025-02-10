37f35aa6bffdea618ff069cdc24bfceda37edd2d
angel4uu <angela.jara@unmsm.edu.pe>
2025-02-10 04:00:53 -0500

    add params to custom buttons

--- src/components/button/AcceptButton.jsx
+++ src/components/button/AcceptButton.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Button } from "../ui/button";
 
-export const AcceptButton = ({ children }) => {
+export const AcceptButton = ({ children, onClick, type }) => {
   return (
     <Button
       className="bg-white text-green-500 border border-green-500 rounded hover:cursor-pointer hover:bg-green-50 py-4 px-7"
       size="sm"
+      onClick={onClick}
+      type={type}
     >
       {children}
     </Button>
--- src/components/button/AddButton.jsx
+++ src/components/button/AddButton.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Button } from "../ui/button";
 import { Plus } from "lucide-react";
 
-export const AddButton = ({ children }) => {
+export const AddButton = ({ children, onClick, type }) => {
   return (
     <Button
-      className="bg-primary rounded hover:cursor-pointer hover:bg-primary/90 "
+      className="bg-primary rounded hover:cursor-pointer hover:bg-primary/90"
       size="sm"
+      onClick={onClick}
+      type={type}
     >
       <Plus />
       {children}
