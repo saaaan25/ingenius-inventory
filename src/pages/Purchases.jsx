3bc2bcfea00c5a6daf6c75551700f7604aefce83
angel4uu <angela.jara@unmsm.edu.pe>
2025-02-10 00:35:34 -0500

    install shadcn

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -1,9 +1,11 @@
+import {Button} from '@/components/ui/button';
 const Purchases = () => {
     return (
         <div className="flex flex-col w-full h-full items-start justify-start gap-y-3">
             <a className="font-light text-routes text-sm" href="/purchases">Compras</a>
             <div className="pl-5">
                 <h1 className="font-semibold text-xl">Compras</h1>
+                <Button>hola</Button>
             </div>
         </div>
     );
