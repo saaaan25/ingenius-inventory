579f39fd330655ce420288be301e615264b83ebb
FranzARCH <franzchipana2610@gmail.com>
2025-02-15 23:27:56 -0500

    data lista utiles y detalle lista

---
+++ src/data-test/listaUtiles.js
@@ -0,0 +1,32 @@
+export const lista_utiles = [
+    {
+        id: 1,
+        nombre: "Lista 1° Primaria",
+        monto_total: 150.00
+    },
+    {
+        id: 2,
+        nombre: "Lista 2° Primaria",
+        monto_total: 160.00
+    },
+    {
+        id: 3,
+        nombre: "Lista 3° Primaria",
+        monto_total: 170.00
+    },
+    {
+        id: 4,
+        nombre: "Lista 4° Primaria",
+        monto_total: 180.00
+    },
+    {
+        id: 5,
+        nombre: "Lista 5° Primaria",
+        monto_total: 190.00
+    },
+    {
+        id: 6,
+        nombre: "Lista 6° Primaria",
+        monto_total: 200.00
+    }
+];
