89a4915e0de6903735dfa4d28b9d45502b2fd767
sofia <ssarmiento088@gmail.com>
2025-02-20 00:24:40 -0500

    adding PageRoute

--- src/pages/Requests.jsx
+++ src/pages/Requests.jsx
@@ -3,8 +3,14 @@ import NavBar from "@/components/ui/NavBar";
 import RequestsSection from "@/components/request/RequestsSection";
 import useActiveTab from "@/hooks/setActiveTab";
 import { useMemo } from "react";
+import PageRoute from "@/components/PageRoute";
 
 const Requests = () => {
+    const page = {
+        name: "Solicitudes",
+        route: "/requests"
+    }
+
     const options = useMemo(() => [
         {
             id: "Pendientes",
@@ -22,7 +28,7 @@ const Requests = () => {
 
     return (
         <div className="flex flex-col w-full h-full items-start justify-start gap-y-3">
-            <a className="font-light text-routes text-sm" href="/requests">Solicitudes</a>
+            <PageRoute page1={page} />
             <div className="pl-5 w-full flex flex-col items-start">
                 <h1 className="font-semibold text-xl mb-2">Solicitudes</h1>
                 <NavBar options={options} active={activeTab} setActive={setActiveTab}/>
