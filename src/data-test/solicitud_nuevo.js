366400341798ebca299713e507f94a9df4e86522
FranzARCH <franzchipana2610@gmail.com>
2025-02-27 12:12:37 -0500

    datatest_nuevo

---
+++ src/data-test/solicitud_nuevo.js
@@ -0,0 +1,37 @@
+const requests_nuevo = [
+    {
+        request_id: 1,
+        user: 103,
+        classroom: "3 años",
+        date: "2023-11-15T00:00:00Z",
+        justification: "Manualidades de otoño",
+        status: "pendiente"
+    },
+    {
+        request_id: 2,
+        user: 104,
+        classroom: "4to primaria",
+        date: "2023-11-20T00:00:00Z",
+        justification: "Proyecto de ciencias",
+        status: "pendiente"
+    },
+    {
+        request_id: 3,
+        user: 103, 
+        classroom: "5to primaria",
+        date: "2023-11-25T00:00:00Z",
+        justification: "Exposición de historia",
+        status: "aceptado"
+    },
+    {
+        request_id: 4,
+        user: 104, 
+        classroom: "6to primaria",
+        date: "2023-11-30T00:00:00Z",
+        justification: "Feria matemática",
+        status: "rechazado"
+    }
+];
+
+export default requests_nuevo;
+
