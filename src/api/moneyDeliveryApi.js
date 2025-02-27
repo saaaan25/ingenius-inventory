01554c99c97874941a7100297ac483ee53345c9c
sofia <ssarmiento088@gmail.com>
2025-02-27 01:19:21 -0500

    updating API requests, adding useMoneyDeliveries hook

---
+++ src/api/moneyDeliveryApi.js
@@ -0,0 +1,28 @@
+import {api} from "@/api";
+
+const moneyDeliveryEndpoint = "/api/money-delivery";
+
+export const getMoneyDeliveries = async () => {
+  const {data}= await api.get(moneyDeliveryEndpoint);
+  return data;
+}
+
+export const createMoneyDelivery = async (moneyDelivery) => {
+  const {data}= await api.post(moneyDeliveryEndpoint, moneyDelivery);
+  return data;
+}
+
+export const updateMoneyDelivery = async (moneyDelivery) => {
+  const {data}= await api.patch(moneyDeliveryEndpoint, moneyDelivery);
+  return data;
+}
+
+export const deleteMoneyDelivery = async (id) => {
+  const {data}= await api.delete(`${moneyDeliveryEndpoint}/${id}`);
+  return data;
+}
+
+export const getMoneyDelivery = async (id) => {
+  const {data}= await api.get(`${moneyDeliveryEndpoint}/${id}`);
+  return data;
+}
